f5b249e9806b0f6f979bee74ac8dbe9aa3238e82
lex-marie790 <lexi.marie790@gmail.com>
2020-06-01 19:54:49 -0700

    actions create

---
+++ smurfs/src/Actions/actions.js
@@ -0,0 +1,13 @@
+export const ADD_SMURF = "ADD_SMURF";
+export const ADD_SMURF_SUCCESS = "ADD_SMURF_SUCCESSFULL";
+export const ADD_SMRUF_FAIL = "ADD_SMURF_FAIL";
+
+export const getSmurfs = () => dispatch => {
+    dispatch({ ADD_SMURF });
+    axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => 
+        dispatch({ ADD_SMURF_SUCCESS, payload: res.data.results })
+        )
+        .catch(err => dispatch({ type: ADD_SMRUF_FAIL, payload: err }));
+};
