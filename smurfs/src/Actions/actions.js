93b629972c86dfa466e6b03e5d900a880e7bb106
lex-marie790 <lexi.marie790@gmail.com>
2020-06-04 17:27:17 -0700

    provider updated

--- smurfs/src/Actions/actions.js
+++ smurfs/src/Actions/actions.js
@@ -1,6 +1,8 @@
+import axios from 'axios';
 export const ADD_SMURF = "ADD_SMURF";
 export const ADD_SMURF_SUCCESS = "ADD_SMURF_SUCCESSFULL";
 export const ADD_SMRUF_FAIL = "ADD_SMURF_FAIL";
+
 
 export const getSmurfs = () => dispatch => {
     dispatch({ ADD_SMURF });
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore, applyMiddleware } from 'redux';
+import smurfReducer from './Reducers/reducers'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const store = createStore(smurfReducer, applyMiddleware(thunk));
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+        document.getElementById("root"));
